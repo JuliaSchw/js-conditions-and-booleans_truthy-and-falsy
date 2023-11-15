1d968349a3ab92fedbb9855ca6dcc145648d1b29
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-11-15 15:45:52 +0100

    challenge 2

--- js/index.js
+++ js/index.js
@@ -1,2 +1,35 @@
-const truthyOrFalsy = Boolean(3 >= 2);
+var truthyOrFalsy = Boolean(2 == "2");
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean(2 === "2");
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean(2 == 2);
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean("Hello world");
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean("");
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean(true);
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean(0);
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean(-50);
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean(50);
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean(5 > 1);
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean("1 > 10");
+console.log("The expression is " + truthyOrFalsy);
+
+var truthyOrFalsy = Boolean("false");
 console.log("The expression is " + truthyOrFalsy);
